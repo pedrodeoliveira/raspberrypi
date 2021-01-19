92442cc638b29c0780850f99af9a78446ee29ecf
Pedro Oliveira <pedro.sim.oliveira@gmail.com>
2021-01-19 00:03:28 +0000

    Code reorganization and added test scripts for camera module.

---
+++ camera/test_picamera.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.rotation = 180
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Pictures/test.jpg')
+camera.stop_preview() 
